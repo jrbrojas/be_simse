fc8c4eb9364f1d17cf7d24138df51749a1ed38c8
Kenyi Valladolid <valladolidk03@gmail.com>
2025-10-01 18:55:34 -0500

    :rocket: Update Evaluacion

--- app/Http/Controllers/Evaluacion/ResumenController.php
+++ app/Http/Controllers/Evaluacion/ResumenController.php
@@ -8,6 +8,7 @@ use App\Models\Monitoreo\EntidadRegistrada as MEntidadRegistrada;
 use App\Models\Seguimiento\EntidadRegistrada as SEntidadRegistrada;
 use App\Models\Supervision\SupervisionEntidadRegistrada;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ResumenController extends Controller
 {
@@ -17,18 +18,25 @@ class ResumenController extends Controller
     {
 
         if ($categoria) {
-            return MEntidadRegistrada::query()
-                ->with([
-                    'entidad',
-                    'distrito',
-                    'provincia',
-                    'departamento',
-                    'categoria',
-                    'respuestas.files'
-                ])
-                ->where('categoria_responsable_id', $categoria)
-                ->orderBy('id', 'desc')
-                ->first();
+
+            $ids = MEntidadRegistrada::query()
+                ->select(DB::raw('MAX(id) as max_id'))
+                ->groupBy('entidad_id')
+                ->get();
+
+            $ids = $ids->pluck('max_id')->toArray();
+
+            return MEntidadRegistrada::with([
+                'entidad',
+                'respuestas',
+                'departamento',
+                'provincia',
+                'distrito'
+            ])
+                ->when(request()->get("categoria"), function ($query, $categoria) {
+                    $query->where('categoria_responsable_id', $categoria);
+                })
+                ->whereIn('id', $ids)->get();
         }
 
         return MEntidadRegistrada::query()
@@ -41,18 +49,28 @@ class ResumenController extends Controller
     public function getSeguimiento(?int $categoria, ?int $entidad_id): SEntidadRegistrada | null
     {
         if ($categoria) {
-            return SEntidadRegistrada::query()
-                ->with([
-                    'entidad',
-                    'distrito',
-                    'provincia',
-                    'departamento',
-                    'respuestas.files',
-                    'categoria',
-                ])
-                ->where('categoria_responsable_id', $categoria)
-                ->orderBy('id', 'desc')
-                ->first();
+            // Obtenemos el último registro por entidad_id
+            $ids = SEntidadRegistrada::query()
+                ->select(DB::raw('MAX(id) as max_id'))
+                ->groupBy('entidad_id')
+                ->pluck('max_id')
+                ->toArray();
+
+            // Devolvemos las entidades con sus relaciones
+            return SEntidadRegistrada::with([
+                'entidad',
+                'categoria',
+                'departamento',
+                'respuestas',
+                'provincia',
+                'distrito',
+                'file'
+            ])
+                ->when(request()->get('categoria'), function ($query, $categoria) {
+                    $query->where('categoria_responsable_id', $categoria);
+                })
+                ->whereIn('id', $ids)
+                ->get();
         }
 
         return SEntidadRegistrada::query()
@@ -68,19 +86,28 @@ class ResumenController extends Controller
     public function getSupervision(?int $categoria, ?int $entidad_id): ?SupervisionEntidadRegistrada
     {
 
-        if($categoria){
-            return SupervisionEntidadRegistrada::query()
-                ->with([
-                    'entidad',
-                    'distrito',
-                    'provincia',
-                    'departamento',
-                    'secciones',
-                    'categoria',
-                ])
-                ->where('categoria_responsable_id', $categoria)
-                ->orderBy('id', 'desc')
-                ->first();
+        if ($categoria) {
+            // Obtenemos el último registro por entidad_id
+            $ids = SupervisionEntidadRegistrada::query()
+                ->select(DB::raw('MAX(id) as max_id'))
+                ->groupBy('entidad_id')
+                ->pluck('max_id')
+                ->toArray();
+
+            // Devolvemos las entidades con sus relaciones
+            return SupervisionEntidadRegistrada::with([
+                'entidad',
+                'categoria',
+                'departamento',
+                'secciones',
+                'provincia',
+                'distrito',
+            ])
+                ->when(request()->get('categoria'), function ($query, $categoria) {
+                    $query->where('categoria_responsable_id', $categoria);
+                })
+                ->whereIn('id', $ids)
+                ->get();
         }
 
         return SupervisionEntidadRegistrada::query()
@@ -107,12 +134,12 @@ class ResumenController extends Controller
         ]);
     }
 
-    public function resumenCategoria(Request $request)
+    public function resumenCategoria()
     {
         // Traemos la entidad registrada con todas sus relaciones filtrados por cateogria
-        $monitoreo = $this->getMonitoreo($request->categoria, null);
-        $seguimiento = $this->getSeguimiento($request->categoria, null);
-        $supervision = $this->getSupervision($request->categoria, null);
+        $monitoreo = $this->getMonitoreo(request()->get("categoria"), null);
+        $seguimiento = $this->getSeguimiento(request()->get("categoria"), null);
+        $supervision = $this->getSupervision(request()->get("categoria"), null);
 
         return response()->json([
             "respuestas" => compact(
